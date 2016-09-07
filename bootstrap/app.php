0eefb0044138a4bd982840cf85670f7d885fb98f
Michael <Michael@frsotweb.co.za>
2016-09-07 06:13:29 +0200

    End of prt 13 Auth3 working

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,7 +3,7 @@
 use  Noodlehaus\Config AS Config;
 use  App\Validation\Validator AS Validator;
 use  App\Middleware\ValidationErrorsMiddleware AS ValidationErrors;
-use  App\Middleware\BreadCrumbs AS BreadCrumbs;
+use  App\Middleware\OldInputMiddleware AS OldInPut;
 
 session_start();
 $_SESSION['breadCrumbs'];
@@ -14,15 +14,12 @@ define('INC_ROOT', dirname(__DIR__));
 require __DIR__ . '/../vendor/autoload.php';
 
 $app = new \Slim\App(array(
-    'settings' => array(
-        'displayErrorDetails' => true,
-        'determineRouteBeforeAppMiddleware' => true,
-        'mode'=>file_get_contents(INC_ROOT.'/mode.php')
-    )
+	'settings' => array(
+		'displayErrorDetails' => true,
+		'determineRouteBeforeAppMiddleware' => true,
+		'mode'=>file_get_contents(INC_ROOT.'/mode.php')
+	)
 ));
-
-
-
 
 //Need to get the slim container and add the twig view to it.
 $container = $app->getContainer();
@@ -32,8 +29,8 @@ $container = $app->getContainer();
 
 //SETTING CONFIG OPTIONS TO CONTAINER
 $container['Config'] = function ($container){
-    $Config = new Config(INC_ROOT."/app/config/".$container->get('settings')['mode'].".php");
-    return $Config;
+	$Config = new Config(INC_ROOT."/app/config/".$container->get('settings')['mode'].".php");
+	return $Config;
 };
 
 /*
@@ -44,48 +41,51 @@ TO GET ITEMS OUT OF THE CONFIG FILE
 
 //SETTING VIEW PARAMS TO CONTAINER
 $container['view'] = function ($container){
-    $view = new \Slim\Views\Twig(
-                             __DIR__.'/../resources/views',
-                            array(
-                                'cache' => false,
-                            )
-    );
+	$view = new \Slim\Views\Twig(
+							 __DIR__.'/../resources/views',
+							array(
+								'cache' => false,
+							)
+	);
 
-    $view->addExtension(new \Slim\Views\TwigExtension(
-        $container->router,
-        $container->request->getUri()
-    ));
+	$view->addExtension(new \Slim\Views\TwigExtension(
+		$container->router,
+		$container->request->getUri()
+	));
 
-    return $view;
+	return $view;
 };
 
 //DATABASE SETUP
 require INC_ROOT."/app/db/database.php";
 $container['db'] = function ($container) use ($capsule){
-    return $capsule;
+	return $capsule;
 };
 
 //VALIDATOR
 $container['validator'] = function ($container) {
 
-    return new Validator();
+	return new Validator();
 
 };
 
 //ADDING MIDDLEWARE TO THE APPLICATION
 $app->add(new ValidationErrors($container));
 
+//PASSES THE OLD FROM DATA BACK TO THE FORM FOR SIGNUP / REGISTER PAGE
+$app->add(new OldInPut($container));
+
 
 /*////////////////////////ROUTES SECTION////////////////////////////////*/
 //HOME CONTROLLER FOR HOME PAGE
 $container['HomeController'] = function ($container) {
-    return new App\Controllers\HomeController($container);
+	return new App\Controllers\HomeController($container);
 };
 
 
 //AUTH CONTROLLER FOR SIGNUP PAGE
 $container['AuthController'] = function ($container) {
-    return new App\Controllers\Auth\AuthController($container);
+	return new App\Controllers\Auth\AuthController($container);
 };
 
 
