61c22573391a144105bec2ef6618825a6871f939
Michael <Michael@frsotweb.co.za>
2016-09-27 14:51:57 +0200

    End of prt 19  dynamically add profile page #TODO # Thank you email chain # Profile page # Update info # Show users # Add other pages

--- app/Controllers/Auth/ProfileController.php
+++ app/Controllers/Auth/ProfileController.php
@@ -5,17 +5,15 @@ namespace App\Controllers\Auth;
 
 
 use App\Controllers\Controller;
+use App\Models\User;
 
 class ProfileController extends Controller
 {
 	public function getViewProfile($request,$response,$agrs)
 	{
+		//GET THE CURRENT LOGGED IN USER INFO
+		$user = User::join('admin_type','user.fk_admin_type','=','admin_type.id')->select('user.username','user.first_name','user.created_at','user.last_name','user.email','admin_type.name as admin_type')->where('user.id',$_SESSION['user'])->first();
 	
-		$names = array();
-		$names[] = array("name"=>"one");
-		$names[] = array("name"=>"two");
-		$names[] = array("name"=>"three");
-	
-		return $this->view->render($response,'/auth/profile.twig', array('test'=>($names)));
+		return $this->view->render($response,'/auth/profile.twig', array('user'=>($user)));
 	}
 }
--- public/js/main.js
+++ public/js/main.js
@@ -13,10 +13,8 @@
 $(function () {
     
     dynamicActiveClass();
-    
-    //NEXT CODE
-  
-  
+    googleAnalyrics();
+
 });
 
 // DYNAMICALLY ADD ACTIVE CLASS TO MENU ITEM
@@ -25,4 +23,15 @@ function dynamicActiveClass() {
     var pathNameOfCurrentPage = window.location.pathname;
     $('a[href="' + pathNameOfCurrentPage + '"]').parent().addClass('active');
     
+}
+
+//GOOGLE ANALYTICS
+function googleAnalyrics() {
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+    
+    ga('create', 'UA-84805393-1', 'auto');
+    ga('send', 'pageview');
 }
