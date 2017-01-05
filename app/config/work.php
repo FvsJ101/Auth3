f31d47f9e30805438555d8d07f0be4268a83e6e2
Michael <Michael@frsotweb.co.za>
2017-01-05 08:16:17 +0200

    final

--- app/config/work.php
+++ app/config/work.php
@@ -44,6 +44,7 @@ return array(
 	'csrf' => array(
 		'key' => 'csrf_token'
 	),
+	
 	'mailchimp' => array(
 		'api_key'     => '91ce56b2213df17f59e3986a8c0548a4-us14',
 		'web_listing' => '62967cf0c5'
